83d89f632894cbc3f62c071b6892ac8761b2765a
jinseo.lee <jinseo0992@gmail.com>
2020-05-11 23:06:02 +0900

    change UI based on work state

--- lib/models/work_data.dart
+++ lib/models/work_data.dart
@@ -1,6 +1,31 @@
 import 'package:flutter/foundation.dart';
 
 class WorkData extends ChangeNotifier {
-  String testData;
+  bool _workState = false;
+  String _testData;
+  int _count = 0;
+
+  int get count {
+    return _count;
+  }
+
+  bool get workState {
+    return _workState;
+  }
+
+  String get testData {
+    return _testData;
+  }
+
+  void updateWorkState() {
+    _workState = !_workState;
+    _count += 1;
+    notifyListeners();
+  }
+
+  void updateTestData(String newTestData) {
+    _testData = newTestData;
+    notifyListeners();
+  }
 
 }
--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -4,6 +4,10 @@ import 'package:provider/provider.dart';
 import 'package:work_timer/models/work_data.dart';
 
 class MainScreen extends StatelessWidget {
+  runTimer(BuildContext context) {
+    Provider.of<WorkData>(context).updateWorkState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,28 +15,33 @@ class MainScreen extends StatelessWidget {
         title: Text('WorkTimer'),
       ),
       body: Container(
-        color: Colors.lightBlueAccent,
+        color:  Provider.of<WorkData>(context).workState ? Colors.lightBlueAccent : Colors.grey,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Expanded(
               flex: 5,
               child: Container(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Icon(
-                      Icons.directions_run,
-                      size: 100.0,
-                    ),
-                    Text(
-                      '00:00',
-                      style: TextStyle(
-                        fontSize: 100.0,
-                        fontWeight: FontWeight.w900,
+                child: GestureDetector(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Provider.of<WorkData>(context).workState ? Icons.directions_run : Icons.airline_seat_legroom_extra,
+                        size: 100.0,
                       ),
-                    ),
-                  ],
+                      Text(
+                        '00:00',
+                        style: TextStyle(
+                          fontSize: 100.0,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                    ],
+                  ),
+                  onTap: () {
+                    runTimer(context);
+                  },
                 ),
               ),
             ),
@@ -48,9 +57,11 @@ class MainScreen extends StatelessWidget {
                   ),
                   onPressed: () {
                     print(1);
-                    Provider.of<WorkData>(context).testData = "main_data";
-                    Navigator.push(context, MaterialPageRoute(
-                        builder: (context) => ResultScreen()));
+                    Provider.of<WorkData>(context).updateTestData("main_data");
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ResultScreen()));
                   },
                 ),
               ),
@@ -63,34 +74,14 @@ class MainScreen extends StatelessWidget {
                     Expanded(
                       child: Row(
                         children: <Widget>[
-                          Container(
-                            child: SizedBox(
-                              height: 10.0,
-                              width: 10.0,
+                          for (int i = 0 ; i < Provider.of<WorkData>(context).count ; i ++)
+                            Container(
+                              child: SizedBox(
+                                height: 10.0,
+                                width: 10.0,
+                              ),
+                              color: i % 2 == 0 ? Colors.lightBlueAccent : Colors.grey,
                             ),
-                            color: Colors.black,
-                          ),
-                          Container(
-                            child: SizedBox(
-                              height: 10.0,
-                              width: 10.0,
-                            ),
-                            color: Colors.green,
-                          ),
-                          Container(
-                            child: SizedBox(
-                              height: 10.0,
-                              width: 10.0,
-                            ),
-                            color: Colors.black,
-                          ),
-                          Container(
-                            child: SizedBox(
-                              height: 10.0,
-                              width: 10.0,
-                            ),
-                            color: Colors.green,
-                          ),
                         ],
                       ),
                     ),
@@ -114,8 +105,8 @@ class MainScreen extends StatelessWidget {
                           ),
                           Row(
                             children: <Widget>[
-                              Icon(
-                                  Icons.airline_seat_legroom_extra, size: 30.0),
+                              Icon(Icons.airline_seat_legroom_extra,
+                                  size: 30.0),
                               Text(
                                 '00:00',
                                 style: TextStyle(
