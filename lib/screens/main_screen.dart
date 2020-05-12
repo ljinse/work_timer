81f28056bb98c6496808cadde42bc403250f5dd3
jinseo.lee <jinseo0992@gmail.com>
2020-05-12 09:48:01 +0900

    apply timer in main screen

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -2,10 +2,96 @@ import 'package:flutter/material.dart';
 import 'package:work_timer/screens/result_screen.dart';
 import 'package:provider/provider.dart';
 import 'package:work_timer/models/work_data.dart';
+import 'dart:async';
 
-class MainScreen extends StatelessWidget {
-  runTimer(BuildContext context) {
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+
+  Stopwatch watch = new Stopwatch();
+  Timer timer;
+  String elapsedTime = 'START';
+  bool is_working = false;
+
+  int workTime = 0;
+  int restTime = 0;
+
+  void startWatch() {
+    setState(() {
+      watch.start();
+      timer = Timer.periodic(Duration(milliseconds: 100), updateTime);
+    });
+  }
+
+  void stopWatch() {
+    setState(() {
+      watch.stop();
+      setTime();
+    });
+  }
+
+  void setTime() {
+    var timeSoFar = watch.elapsedMilliseconds;
+    setState(() {
+      elapsedTime = transformMilliSeconds(timeSoFar, false);
+    });
+  }
+
+  void updateTime(Timer timer) {
+    if (watch.isRunning) {
+      setState(() {
+        //print("startstop Inside=$startStop");
+        elapsedTime = transformMilliSeconds(watch.elapsedMilliseconds, false);
+      });
+    }
+  }
+
+  void runTimer(BuildContext context) {
     Provider.of<WorkData>(context).updateWorkState();
+  }
+
+  void handleTap() {
+    if (watch.isRunning) {
+      stopWatch();
+    }
+
+    // reset
+    setState(() {
+      if (is_working) {
+        workTime += watch.elapsedMilliseconds;
+      } else {
+        restTime += watch.elapsedMilliseconds;
+      }
+      is_working = !is_working;
+    });
+
+    watch.reset();
+
+    //restart
+    startWatch();
+    print("startstop Inside=$is_working");
+    elapsedTime = transformMilliSeconds(watch.elapsedMilliseconds, false);
+
+  }
+
+  String transformMilliSeconds(int milliseconds, bool includeHours) {
+    int hundreds = (milliseconds / 10).truncate();
+    int seconds = (hundreds / 100).truncate();
+    int minutes = (seconds / 60).truncate();
+    int hours = (minutes / 60).truncate();
+
+    String hoursStr = (hours % 24).toString().padLeft(2, '0');
+    String minutesStr = (minutes % 60).toString().padLeft(2, '0');
+    String secondsStr = (seconds % 60).toString().padLeft(2, '0');
+    //String hundredsStr = (hundreds % 100).toString().padLeft(2, '0');
+
+    if (includeHours) {
+      return "$hoursStr:$minutesStr:$secondsStr";
+    }
+    return "$minutesStr:$secondsStr";
   }
 
   @override
@@ -31,7 +117,7 @@ class MainScreen extends StatelessWidget {
                         size: 100.0,
                       ),
                       Text(
-                        '00:00',
+                        elapsedTime,
                         style: TextStyle(
                           fontSize: 100.0,
                           fontWeight: FontWeight.w900,
@@ -40,6 +126,7 @@ class MainScreen extends StatelessWidget {
                     ],
                   ),
                   onTap: () {
+                    handleTap();
                     runTimer(context);
                   },
                 ),
@@ -74,7 +161,7 @@ class MainScreen extends StatelessWidget {
                     Expanded(
                       child: Row(
                         children: <Widget>[
-                          for (int i = 0 ; i < Provider.of<WorkData>(context).count ; i ++)
+                          for (int i = 0 ; i < Provider.of<WorkData>(context).count -1 ; i ++)
                             Container(
                               child: SizedBox(
                                 height: 10.0,
@@ -96,7 +183,7 @@ class MainScreen extends StatelessWidget {
                                 size: 30.0,
                               ),
                               Text(
-                                '00:00',
+                                transformMilliSeconds(workTime, true),
                                 style: TextStyle(
                                   fontSize: 30.0,
                                 ),
@@ -108,7 +195,7 @@ class MainScreen extends StatelessWidget {
                               Icon(Icons.airline_seat_legroom_extra,
                                   size: 30.0),
                               Text(
-                                '00:00',
+                                transformMilliSeconds(restTime, true),
                                 style: TextStyle(
                                   fontSize: 30.0,
                                 ),
